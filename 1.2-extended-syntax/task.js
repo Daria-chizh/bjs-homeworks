83e9aa67eef188b811ec5495131d068273aadbd2
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-03-25 21:16:02 +0700

    implement second task for #2.1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -15,8 +15,21 @@ function getResult(a, b, c){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+  if (marks.length === 0) {
+    return 0;
+  }
+
+  if (marks.length > 5) {
+    console.log('Количество оценок больше 5');
+    marks.splice(5);
+  }
+
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+   sum += marks[i];
+  }
+
+  return (sum / marks.length);
 }
 
 function askDrink(name,dateOfBirthday){
