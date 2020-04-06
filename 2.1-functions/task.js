31a0b5e9d0e5c23952c033d691ad8756e9802ab4
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-04-06 18:57:16 +0700

    fix square roots calculation (operators precedence)

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -4,11 +4,11 @@ function getSolutions(a, b, c) {
   if (D < 0) {
     return { D: D, roots: [] };
   } else if (D === 0) {
-    let x1 = -b / 2 * a;
+    let x1 = -b / (2 * a);
     return { D: D, roots: [x1] };
   } else if (D > 0) {
-    let x1 = (-b + Math.sqrt(D)) / 2 * a;
-    let x2 = (-b - Math.sqrt(D)) / 2 * a;
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
     return { D: D, roots: [x1, x2] };
   }
 }
